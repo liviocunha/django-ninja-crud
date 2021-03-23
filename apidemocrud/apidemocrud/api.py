56fa2103d9f570ece5b6f5318951b5c69e46a372
Lívio Cunha <liviocunha91@hotmail.com>
2021-03-23 18:37:46 -0300

    Chore: Create Django project and config python-decouple

---
+++ apidemocrud/apidemocrud/api.py
@@ -0,0 +1,18 @@
+from datetime import date
+from ninja import Schema
+
+
+class EmployeeIn(Schema):
+    first_name: str
+    last_name: str
+    department_id: int = None
+    birthdate: date = None
+
+
+class EmployeeOut(Schema):
+    id: int
+    first_name: str
+    last_name: str
+    department_id: int = None
+    birthdate: date = None
+
