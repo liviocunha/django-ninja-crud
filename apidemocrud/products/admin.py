4f680c40a29e737b9533818ab21fbed40e4b8383
Lívio Cunha <liviocunha91@hotmail.com>
2021-03-23 19:37:14 -0300

    Feat: Activate admin for products and department

--- apidemocrud/products/admin.py
+++ apidemocrud/products/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from .models import Department, Product
 
-# Register your models here.
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('sku', 'title', 'stock', 'department')
+    search_fields = ('sku', 'title')
+
+
+class DepartmentAdmin(admin.ModelAdmin):
+    list_display = ('title',)
+    search_fields = ('title',)
+
+
+admin.site.register(Department, DepartmentAdmin)
+admin.site.register(Product, ProductAdmin)
