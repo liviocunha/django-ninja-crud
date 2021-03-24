96adb5d2f3eda8694783318d7e8dd8a284b695b7
Lívio Cunha <liviocunha91@hotmail.com>
2021-03-24 10:26:26 -0300

    Feat: Create function list departments and products

--- apidemocrud/products/views.py
+++ apidemocrud/products/views.py
@@ -1,6 +1,8 @@
 from ninja import Router
+from typing import List
 from .schemas import ProductsIn, ProductsOut, DepartmentIn, DepartmentOut
 from .models import Product, Department
+
 
 router_api = Router()
 
@@ -15,3 +17,15 @@ def create_product(request, payload: ProductsIn):
 def create_department(request, payload: DepartmentIn):
     department = Department.objects.create(**payload.dict())
     return {"id": department.id}
+
+
+@router_api.get("/product", response=List[ProductsOut])
+def list_product(request):
+    qs = Product.objects.all()
+    return qs
+
+
+@router_api.get("/department", response=List[DepartmentOut])
+def list_department(request):
+    qs = Department.objects.all()
+    return qs
