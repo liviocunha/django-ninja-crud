ee541616f8a9796ac3d8917dc2a0de1211b2d9db
Lívio Cunha <liviocunha91@hotmail.com>
2021-03-24 10:37:24 -0300

    Feat: Create function get department and product

--- apidemocrud/products/views.py
+++ apidemocrud/products/views.py
@@ -2,6 +2,7 @@ from ninja import Router
 from typing import List
 from .schemas import ProductsIn, ProductsOut, DepartmentIn, DepartmentOut
 from .models import Product, Department
+from django.shortcuts import get_object_or_404
 
 
 router_api = Router()
@@ -29,3 +30,15 @@ def list_product(request):
 def list_department(request):
     qs = Department.objects.all()
     return qs
+
+
+@router_api.get("/department/{department_id}", response=DepartmentOut)
+def get_department(request, department_id: int):
+    department = get_object_or_404(Department, id=department_id)
+    return department
+
+
+@router_api.get("/product/{sku}", response=ProductsOut)
+def get_product(request, sku: str):
+    product = get_object_or_404(Product, sku=sku)
+    return product
