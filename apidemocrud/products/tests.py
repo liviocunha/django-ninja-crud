cb89060f29af9781bf5a1a600d47d3bdaad2c5c5
Lívio Cunha <liviocunha91@hotmail.com>
2021-11-16 23:27:52 -0300

    Test: Create TestCase, setUp and test auth

--- apidemocrud/products/tests.py
+++ apidemocrud/products/tests.py
@@ -1,3 +1,21 @@
 from django.test import TestCase
+from faker import Faker
+from model_mommy import mommy
+from ninja.testing import TestClient
 
-# Create your tests here.
+from apidemocrud.products.api import router_api
+from apidemocrud.products.models import Client
+
+client = TestClient(router_api)
+fake = Faker()
+
+
+class TestesDemoCrud(TestCase):
+    def setUp(self):
+        self.client_user = mommy.make(Client,
+                                      name=fake.name,
+                                      key=fake.uuid4())
+
+    def test_auth(self):
+        response = client.get(f"/auth?api_key={self.client_user.key}")
+        self.assertEqual(response.json(), f"Authenticated {self.client_user.name}")
